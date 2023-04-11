1998ec461c9e340fd70836f05c79e0d723343c57
jumanazarotamurodov <jotamurodov8@gmail.com>
2023-04-11 15:45:46 +0500

    ism taxriri

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function tes(){
+  const name = "Jumanazar";
+  let pro = prompt("ismingizni kiriting");
+  if (pro === name){
+    alert("Ismingiz qabul qilindi")
+  }else{
+    alert("kechirasiz qaytattan kiriting")
+  }
+}
